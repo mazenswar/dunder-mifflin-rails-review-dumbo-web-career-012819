541dd2c06fe9521213e7e22b8a0565814e392541
mazenswar <mazenswar@protonmail.com>
2019-02-28 15:44:28 -0500

    dunder miffin

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,23 @@
 class DogsController < ApplicationController
+
+  def index
+    @dogs = Dog.all
+    # @dogs.sort_by(:&employees)
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+  end
+
+  def sort
+    @dogs = Dog.all
+    @dogs = @dogs.sort_by do |dog|
+      dog.employees.length
+    end
+
+    byebug
+
+    render :index
+  end
+
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,43 @@
 class EmployeesController < ApplicationController
+
+  def index
+    @employees = Employee.all
+  end
+
+  def show
+    @employee = Employee.find(params[:id])
+  end
+
+  def new
+    @employee = Employee.new
+  end
+
+  def create
+    @employee = Employee.create(employee_params)
+    if @employee.valid?
+      redirect_to employee_path(@employee)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @employee = Employee.find(params[:id])
+  end
+
+  def update
+    @employee = Employee.update(employee_params)
+    if @employee.valid?
+      redirect_to employee_path(@employee)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
+  end
+
 end
